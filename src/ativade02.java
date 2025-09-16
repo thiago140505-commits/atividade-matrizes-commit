f2fb2ce9b7a06f928764dd3bd970f1a9c0973678
Thiago <thiago140505@edu.unipar.br>
2025-09-16 11:40:26 -0300

    criei um vetor para armazenar 15 numeros inteiros, e usei o random para preencher com numeros aleatorios de 1 a 100, e para gerar os numeros criei um for, e no segundo for ele faz a conta de divisão por 2 em numero por numero para saber ver qual é impar e qual é par, printando todos os numeros

---
+++ src/ativade02.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class ativade02 {
+    public static void main(String[] args) {
+
+        Random gerador = new Random();
+        int[] numeros = new int[15];
+
+        for (int i = 0; i < numeros.length; i++){
+            numeros[i] = gerador.nextInt(100) + 1 ;
+            System.out.println(numeros[i] + " ");
+        }
+
+
+        System.out.println("Numeros pares e impares:");
+        for (int numero : numeros) {
+            if (numero % 2 == 0 ) {
+            System.out.println(numero + " É par");
+            } else {
+            System.out.println(numero + " È impar");
+
+            }
+        }
+    }
+}
