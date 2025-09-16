0771f12db93fb2494121539e646e4d425cb0737e
Thiago <thiago140505@edu.unipar.br>
2025-09-16 11:40:26 -0300

    Criamos um vetor que guarda 10 numeros inteiros, criei um for para armazenar os numeros do usuario, printando os numeros que for digitado, criei mais um for para repetir numero por numero do usuario para o if-else verificar qual o maior numero e o menor numero, printando as respostas

--- src/atividade01.java
+++ src/atividade01.java
@@ -4,7 +4,7 @@ public class atividade01 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int[] numeros = new int[5];
+        int[] numeros = new int[10];
 
         for (int i = 0; i < numeros.length; i++) {
 
